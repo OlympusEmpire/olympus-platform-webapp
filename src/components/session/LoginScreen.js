6c6111be3e48c8b81612920a857dcd2cbde37912
JuliusSauco <chris_kevin8@hotmail.com>
2022-02-22 20:11:55 -0500

    add login style

--- src/components/session/LoginScreen.js
+++ src/components/session/LoginScreen.js
@@ -1,58 +1,56 @@
 import React from 'react'
+import "./login.css"
 
-export const LoginScreen = ({ history }) => {
-
-    const handleLogin = () => {
-        history.replace('/');
-    }
+export const LoginScreen = () => {
 
     return (
         <div
+             id = "loginform"
              className = "container mt-5"
         >
             <form>
 
-            <div class="form-outline mb-4">
-                <input type="email" id="form2Example1" class="form-control " />
-                <label class="form-label text-warning" for="form2Example1">Email o Username</label>
+            <div className="form-outline mb-4">
+                <input type="email" id="form2Example1" className="form-control text-white" />
+                <label className="form-label text-warning" for="form2Example1">Email o Username</label>
             </div>
 
-            <div class="form-outline mb-4">
-                <input type="password" id="form2Example2" class="form-control" />
-                <label class="form-label text-warning" for="form2Example2">Contraseña</label>
+            <div className="form-outline mb-4">
+                <input type="password" id="form2Example2" className="form-control text-white" />
+                <label className="form-label text-warning" for="form2Example2">Contraseña</label>
             </div>
 
-            <div class="row mb-4 text-light">
-                <div class="col d-flex justify-content-center">
+            <div className="row mb-4 text-light">
+                <div className="col d-flex justify-content-center">
 
-                <div class="form-check">
+                <div className="form-check">
                     <input
-                    class="form-check-input bg-dark"
+                    className="form-check-input bg-dark"
                     type="checkbox"
                     id="form2Example34"
                     />
-                    <label class="form-check-label" for="form2Example34"> Recuérdame </label>
+                    <label className="form-check-label" for="form2Example34"> Recuérdame </label>
                 </div>
                 </div>
 
-                <div class="col">
+                <div className="col">
 
                 <a href="#!" className = "text-warning">¿Olvidaste la contraseña?</a>
                 </div>
             </div>
 
-            <button type="submit" class="btn btn-outline-warning btn-block mb-5">Iniciar Sesión</button>
+            <button type="submit" className="btn btn-outline-warning btn-block mb-5">Iniciar Sesión</button>
 
-            <div class="text-center text-light">
+            <div className="text-center text-light">
                 <p>¿Aún no eres miembro? <a href="#!" className = "text-warning"> Registrate</a></p>
                 <p>o inicia sesión con:</p>
 
-                <button type="button" class="btn btn-primary btn-floating mx-1">
-                <i class="fab fa-facebook-f"></i>
+                <button type="button" className="btn btn-primary btn-floating mx-1">
+                <i className="fab fa-facebook-f"></i>
                 </button>
 
-                <button type="button" class="btn btn-danger btn-floating mx-1">
-                <i class="fab fa-google"></i>
+                <button type="button" className="btn btn-danger btn-floating mx-1">
+                <i className="fab fa-google"></i>
                 </button>
 
             </div>
