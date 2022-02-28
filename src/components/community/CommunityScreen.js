7ab5e5593d9984ee4e8c120f54fadde8d920e4ea
JuliusSauco <chris_kevin8@hotmail.com>
2022-02-27 20:13:18 -0500

    Add Communities Screen

--- src/components/community/CommunityScreen.js
+++ src/components/community/CommunityScreen.js
@@ -1,9 +1,142 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import "./community.css"
 
 export const CommunityScreen = () => {
     return (
-        <div>
-            <h1>Communities Screen</h1>
+        <Fragment>
+        <div claasName="text-center">
+            <div className="container p-5">
+                <h1 className="text-center text-white">COMUNIDADES</h1>
+                <p className="text-white text-justify">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+                irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+                deserunt mollit anim id est laborum.
+                </p>
+            </div>
         </div>
+
+        <div className="container" id="cont-cm">
+        <div className="row">
+            
+            <div className="col-sm-4 col-sm-12 col-lg-3">
+            <div className="card team text-white bg-dark cm-card">
+                <h4 name="typeUserName" className="card-title tittle text-center" >OLYMPUS</h4>
+                <img className="card-img-top img-fluid rounded" src="https://i.imgur.com/2P6CgK4.jpg" alt="Card image cap"></img>
+                <div className="card-body md-5">
+                    <h10 name="subtittle" className="card-text subtitle md-1">Grupo de amistad y comunidades.</h10>
+                    <p name="description" className="card-text subtitle md-1"></p>
+                    <a href="#">Ver más...</a>
+                    <button type="button" id="social-btn" className="btn btn-outline-primary btn-lg btn-block">Grupo de Facebook</button>
+                    <button type="button" id="social-btn" className="btn btn-outline-success btn-lg btn-block">Grupo de WhatsApp</button>
+                    <button type="button" id="social-btn" className="btn btn-outline-warning btn-lg btn-block">Grupo de Instagram</button>
+                </div>
+            </div>
+            </div>
+
+            <div className="col-sm-4 col-sm-12 col-lg-3">
+            <div className="card team text-white bg-dark cm-card">
+                <h4 name="typeUserName" className="card-title tittle text-center" >OLYMPUS</h4>
+                <img className="card-img-top img-fluid rounded" src="https://i.imgur.com/2P6CgK4.jpg" alt="Card image cap"></img>
+                <div className="card-body md-5">
+                    <p name="typeUserDesc" className="md-1" className="card-text subtitle">Grupo de amistad y comunidades.</p>
+                    <a href="#">Ver más...</a>
+                    <button type="button" id="social-btn" className="btn btn-outline-primary btn-lg btn-block">Grupo de Facebook</button>
+                    <button type="button" id="social-btn" className="btn btn-outline-success btn-lg btn-block">Grupo de WhatsApp</button>
+                    <button type="button" id="social-btn" className="btn btn-outline-warning btn-lg btn-block">Grupo de Instagram</button>
+                </div>
+            </div>
+            </div>
+
+
+            <div className="col-sm-4 col-sm-12 col-lg-3">
+            <div className="card team text-white bg-dark cm-card">
+                <h4 name="typeUserName" className="card-title tittle text-center" >OLYMPUS</h4>
+                <img className="card-img-top img-fluid rounded" src="https://i.imgur.com/2P6CgK4.jpg" alt="Card image cap"></img>
+                <div className="card-body md-5">
+                    <p name="typeUserDesc" className="md-1" className="card-text subtitle">Grupo de amistad y comunidades.</p>
+                    <a href="#">Ver más...</a>
+                    <button type="button" id="social-btn" className="btn btn-outline-primary btn-lg btn-block">Grupo de Facebook</button>
+                    <button type="button" id="social-btn" className="btn btn-outline-success btn-lg btn-block">Grupo de WhatsApp</button>
+                    <button type="button" id="social-btn" className="btn btn-outline-warning btn-lg btn-block">Grupo de Instagram</button>
+                </div>
+            </div>
+            </div>
+
+            <div className="col-sm-4 col-sm-12 col-lg-3">
+            <div className="card team text-white bg-dark cm-card">
+                <h4 name="typeUserName" className="card-title tittle text-center" >OLYMPUS</h4>
+                <img className="card-img-top img-fluid rounded" src="https://i.imgur.com/2P6CgK4.jpg" alt="Card image cap"></img>
+                <div className="card-body md-5">
+                    <p name="typeUserDesc" className="md-1" className="card-text subtitle">Grupo de amistad y comunidades.</p>
+                    <a href="#">Ver más...</a>
+                    <button type="button" id="social-btn" className="btn btn-outline-primary btn-lg btn-block">Grupo de Facebook</button>
+                    <button type="button" id="social-btn" className="btn btn-outline-success btn-lg btn-block">Grupo de WhatsApp</button>
+                    <button type="button" id="social-btn" className="btn btn-outline-warning btn-lg btn-block">Grupo de Instagram</button>
+                </div>
+            </div>
+            </div>
+
+            <div className="col-sm-4 col-sm-12 col-lg-3">
+            <div className="card team text-white bg-dark cm-card">
+                <h4 name="typeUserName" className="card-title tittle text-center" >OLYMPUS</h4>
+                <img className="card-img-top img-fluid rounded" src="https://i.imgur.com/2P6CgK4.jpg" alt="Card image cap"></img>
+                <div className="card-body md-5">
+                    <p name="typeUserDesc" className="md-1" className="card-text subtitle">Grupo de amistad y comunidades.</p>
+                    <a href="#">Ver más...</a>
+                    <button type="button" id="social-btn" className="btn btn-outline-primary btn-lg btn-block">Grupo de Facebook</button>
+                    <button type="button" id="social-btn" className="btn btn-outline-success btn-lg btn-block">Grupo de WhatsApp</button>
+                    <button type="button" id="social-btn" className="btn btn-outline-warning btn-lg btn-block">Grupo de Instagram</button>
+                </div>
+            </div>
+            </div>
+
+            <div className="col-sm-4 col-sm-12 col-lg-3">
+            <div className="card team text-white bg-dark cm-card">
+                <h4 name="typeUserName" className="card-title tittle text-center" >OLYMPUS</h4>
+                <img className="card-img-top img-fluid rounded" src="https://i.imgur.com/2P6CgK4.jpg" alt="Card image cap"></img>
+                <div className="card-body md-5">
+                    <p name="typeUserDesc" className="md-1" className="card-text subtitle">Grupo de amistad y comunidades.</p>
+                    <a href="#">Ver más...</a>
+                    <button type="button" id="social-btn" className="btn btn-outline-primary btn-lg btn-block">Grupo de Facebook</button>
+                    <button type="button" id="social-btn" className="btn btn-outline-success btn-lg btn-block">Grupo de WhatsApp</button>
+                    <button type="button" id="social-btn" className="btn btn-outline-warning btn-lg btn-block">Grupo de Instagram</button>
+                </div>
+            </div>
+            </div>
+
+            <div className="col-sm-4 col-sm-12 col-lg-3">
+            <div className="card team text-white bg-dark cm-card">
+                <h4 name="typeUserName" className="card-title tittle text-center" >OLYMPUS</h4>
+                <img className="card-img-top img-fluid rounded" src="https://i.imgur.com/2P6CgK4.jpg" alt="Card image cap"></img>
+                <div className="card-body md-5">
+                    <p name="typeUserDesc" className="md-1" className="card-text subtitle">Grupo de amistad y comunidades.</p>
+                    <a href="#">Ver más...</a>
+                    <button type="button" id="social-btn" className="btn btn-outline-primary btn-lg btn-block">Grupo de Facebook</button>
+                    <button type="button" id="social-btn" className="btn btn-outline-success btn-lg btn-block">Grupo de WhatsApp</button>
+                    <button type="button" id="social-btn" className="btn btn-outline-warning btn-lg btn-block">Grupo de Instagram</button>
+                </div>
+            </div>
+            </div>
+
+            <div className="col-sm-4 col-sm-12 col-lg-3">
+            <div className="card team text-white bg-dark cm-card">
+                <h4 name="typeUserName" className="card-title tittle text-center" >OLYMPUS</h4>
+                <img className="card-img-top img-fluid rounded" src="https://i.imgur.com/2P6CgK4.jpg" alt="Card image cap"></img>
+                <div className="card-body md-5">
+                    <p name="typeUserDesc" className="md-1" className="card-text subtitle">Grupo de amistad y comunidades.</p>
+                    <a href="#">Ver más...</a>
+                    <button type="button" id="social-btn" className="btn btn-outline-primary btn-lg btn-block">Grupo de Facebook</button>
+                    <button type="button" id="social-btn" className="btn btn-outline-success btn-lg btn-block">Grupo de WhatsApp</button>
+                    <button type="button" id="social-btn" className="btn btn-outline-warning btn-lg btn-block">Grupo de Instagram</button>
+                </div>
+            </div>
+            </div>
+
+        </div>
+        </div>
+        </Fragment>
     )
 }
